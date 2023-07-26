fd9cb2d34be505d2e3549b49eb90f169b5000879
visnesuyu <110824350+visnesuyu@users.noreply.github.com>
2023-07-26 16:01:41 +0300

    .Ball movement changed
    Still testing but it seems okay now

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speedY = 5.0f;
+    public float maxXSpeed = 3.0f;
+    private Rigidbody2D ballRb;
+
+    private void Awake()
+    {
+        ballRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        
+        ballRb.velocity = new Vector2(ballRb.velocity.x, speedY);
+    }
+    private void FixedUpdate()
+    {
+        //Xteki hýzýný sabitlemek adýna yazýldý
+        float speedX = Mathf.Abs(ballRb.velocity.x) > maxXSpeed ? maxXSpeed * Mathf.Sign(ballRb.velocity.x) : ballRb.velocity.x;
+    }
+}
