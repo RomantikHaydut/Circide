3f70e7fdae7af8208292ba59e3d92cf622cfdd83
visnesuyu <110824350+visnesuyu@users.noreply.github.com>
2023-07-26 16:37:32 +0300

    .Score text added
    It doesn't shown on game but it is working

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class BallMovement : MonoBehaviour
 {
     public float speedY = 5.0f;
     public float maxXSpeed = 3.0f;
     private Rigidbody2D ballRb;
-
+    private int score = 1; 
+    private TMP_Text scoreText;
     private void Awake()
     {
         ballRb = GetComponent<Rigidbody2D>();
@@ -17,6 +18,8 @@ public class BallMovement : MonoBehaviour
     {
         
         ballRb.velocity = new Vector2(ballRb.velocity.x, speedY);
+        scoreText = GetComponentInChildren<TMP_Text>();
+        
     }
     private void FixedUpdate()
     {
@@ -28,6 +31,18 @@ public class BallMovement : MonoBehaviour
         if(collision.CompareTag("ScoreBoard"))
         {
             gameObject.SetActive(false);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Board") 
+        {
+            score++; 
+            UpdateScoreText(); 
+        }
+        void UpdateScoreText()
+        {
+            scoreText.text = score.ToString(); 
         }
     }
 }
