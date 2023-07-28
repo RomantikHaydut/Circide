7526d1d24f0dcc36f51c3fc90d9b8e3dc51e995d
visnesuyu <110824350+visnesuyu@users.noreply.github.com>
2023-07-28 04:28:35 +0300

    .Score animation added some bug fixed

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -2,47 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 public class BallMovement : MonoBehaviour
 {
     public float speedY = 5.0f;
     public float maxXSpeed = 3.0f;
     private Rigidbody2D ballRb;
-    private int score = 1; 
+    private int score = 1;
     private TMP_Text scoreText;
+    private RectTransform scoreRect;
+    Vector3 rectScale;
+
+
     private void Awake()
     {
         ballRb = GetComponent<Rigidbody2D>();
+        scoreText = GetComponentInChildren<TMP_Text>();
+        scoreRect = GetComponentInChildren<RectTransform>();
+        rectScale = scoreRect.transform.localScale;
+        scoreText.gameObject.SetActive(false);
+        
     }
+
 
     private void Start()
     {
-        
+
         ballRb.velocity = new Vector2(ballRb.velocity.x, speedY);
-        scoreText = GetComponentInChildren<TMP_Text>();
-        
+
+
     }
     private void FixedUpdate()
     {
         //Xteki hýzýný sabitlemek adýna yazýldý
-        float speedX = Mathf.Abs(ballRb.velocity.x) > maxXSpeed ? maxXSpeed * Mathf.Sign(ballRb.velocity.x) : ballRb.velocity.x;
+        //float speedX = Mathf.Abs(ballRb.velocity.x) > maxXSpeed ? maxXSpeed * Mathf.Sign(ballRb.velocity.x) : ballRb.velocity.x;
+        ballRb.velocity = Vector2.ClampMagnitude(ballRb.velocity, maxXSpeed);
+    }
+    void PopUpText()
+    {
+        DOTween.Complete(scoreRect);
+        scoreRect.transform.localScale = rectScale;
+        float duration = 1f;
+        float distancetween = 3f;
+        scoreText.gameObject.SetActive(true);
+        scoreRect.DOAnchorPosY(scoreRect.anchoredPosition.y + distancetween, duration).OnComplete(() =>
+        {
+            scoreRect.anchoredPosition = new Vector2(scoreRect.anchoredPosition.x, scoreRect.anchoredPosition.y - distancetween);
+            scoreText.gameObject.SetActive(false);
+        });
+        scoreRect.transform.DOScale(scoreRect.transform.localScale * 2, duration / 2).OnComplete(() =>
+        {
+            scoreRect.transform.DOScale(Vector3.zero, duration / 2);
+        });
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("ScoreBoard"))
+        if (collision.CompareTag("ScoreBoard"))
         {
             gameObject.SetActive(false);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Board") 
+        if (collision.gameObject.tag == "Board")
         {
-            score++; 
-            UpdateScoreText(); 
+            score++;
+            PopUpText();
+            UpdateScoreText();
+            //popupanimation
         }
         void UpdateScoreText()
         {
-            scoreText.text = score.ToString(); 
+            scoreText.text = score.ToString();
         }
     }
+
 }
--- Assets/Scripts/Jumper.cs
+++ Assets/Scripts/Jumper.cs
@@ -7,7 +7,13 @@ public class Jumper : MonoBehaviour
 
     private Vector2 initialPosition;
     private float deltaX, deltaY;
-
+    private BoxCollider2D ballCollider;
+    bool isColliding;
+    private void Awake()
+    {
+        ballCollider = GetComponent<BoxCollider2D>();
+        ballCollider.isTrigger = true;
+    }
     private void Start()
     {
         initialPosition = transform.position;
@@ -32,10 +38,41 @@ public class Jumper : MonoBehaviour
                     break;
 
                 case TouchPhase.Ended:
-                    transform.position = new Vector2(touchPosition.x - deltaX, touchPosition.y - deltaY);
-                    this.enabled = false;
+                    if (!isColliding)
+                    {
+                        transform.position = new Vector2(touchPosition.x - deltaX, touchPosition.y - deltaY);
+                        ballCollider.enabled = true;
+                        ballCollider.isTrigger = false;
+                        this.enabled = false;
+
+                    }
+                    else
+                    {
+                        YouDidItWrong();
+                    }
                     break;
+
             }
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Jumper xx))
+        {
+            isColliding = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Jumper xx))
+        {
+            isColliding = false;
+        }
+    }
+    void YouDidItWrong()
+    {
+        Destroy(gameObject);
+    }
 }
+
+
